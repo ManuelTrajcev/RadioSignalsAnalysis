e90fa415f61b834c92ddc752c5c1fcd622b80047
Manuel <manueltrajcev7@gmail.com>
2025-08-30 14:23:41 +0200

    FTR-1: Repository Layer
    Generic Repository inteface and implementation

---
+++ RadioSignalsBackEnd/Repository/Interface/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+using Domain.Domain_Models;
+
+namespace Repository.Interface
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Insert(T entity);
+        List<T> InsertMany(List<T> entities);
+        T Update(T entity);
+        T Delete(T entity);
+        E? Get<E>(Expression<Func<T, E>> selector,
+            Expression<Func<T, bool>>? predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null);
+
+        IEnumerable<E> GetAll<E>(Expression<Func<T, E>> selector,
+            Expression<Func<T, bool>>? predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null);
+    }
+}
