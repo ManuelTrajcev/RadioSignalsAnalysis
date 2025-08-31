c4ce277529896ceb3be199a38fd22f91e0bad53a
Manuel <manueltrajcev7@gmail.com>
2025-08-31 11:32:30 +0200

    Frontend update for jwt auth

--- RadioSignalsFrontEnd/src/contexts/authContext.js
+++ RadioSignalsFrontEnd/src/contexts/authContext.js
@@ -1,5 +1,5 @@
-// import React from "react";
-//
-// const authContext = React.createContext(null);
-//
-// export default authContext;
+import React from "react";
+
+const authContext = React.createContext(null);
+
+export default authContext;
