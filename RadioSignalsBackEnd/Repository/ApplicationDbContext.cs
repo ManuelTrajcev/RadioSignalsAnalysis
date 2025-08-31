efe99f532440d27d2682891ff9b1efea8841b616
Manuel <manueltrajcev7@gmail.com>
2025-08-31 10:34:37 +0200

    Db Context and connection string

--- RadioSignalsBackEnd/Repository/ApplicationDbContext.cs
+++ RadioSignalsBackEnd/Repository/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System.Threading.Channels;
+using Domain.Domain_Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace RadioSignalsWeb.Data;
@@ -9,4 +11,13 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    
+    public DbSet<ChannelFrequency> ChannelFrequencies { get; set; }
+    public DbSet<ElectricFieldStrength> ElectricFieldStrengths { get; set; }
+    public DbSet<GeoCoordinate> GeoCoordinates { get; set; }
+    public DbSet<Measurement> Measurements { get; set; }
+    public DbSet<Municipality> Municipalities { get; set; }
+    public DbSet<Settlement> Settlements { get; set; }
+    public DbSet<User> User { get; set; }
+
 }
