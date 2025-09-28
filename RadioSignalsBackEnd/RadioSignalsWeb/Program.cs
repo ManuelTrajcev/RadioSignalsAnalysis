9ae615ca7abd044b23fac77501965bbe63a6f5f2
EndritHasani01 <endrithasani2019@gmail.com>
2025-09-28 19:10:35 +0200

    fix delete, edit error

--- RadioSignalsBackEnd/RadioSignalsWeb/Program.cs
+++ RadioSignalsBackEnd/RadioSignalsWeb/Program.cs
@@ -71,6 +71,7 @@ builder.Services.AddAuthentication(options =>
 })
 .AddJwtBearer(options =>
 {
+    options.MapInboundClaims = false;
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = true,
