e5bf8dfcd4b3cf4e1d3e41d473b269762da17ad7
Manuel <manuel@codechem.com>
2026-06-27 20:36:14 +0200

    test

--- RadioSignalsFrontEnd/src/theme.js
+++ RadioSignalsFrontEnd/src/theme.js
@@ -2,7 +2,7 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
     palette: {
-        primary: { main: '#f44336' },
+        primary: { main: '#2e7d32' },
         secondary: { main: '#ff9800' },
         error: { main: '#d32f2f' },
     }
